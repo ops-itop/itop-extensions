a360ebec13ec45ac4554bd896ead436958b807ad
annprog <tecbbs@qq.com>
2016-05-25 15:47:46 +0800

    :sunny: :relaxed::relaxed::relaxed:

--- le-config-mgmt/zh.dict.le-config-mgmt.php
+++ le-config-mgmt/zh.dict.le-config-mgmt.php
@@ -17,27 +17,25 @@ Dict::Add('ZH CN', 'Chinese', '简体中文', array(
 // Cluster
 Dict::Add('ZH CN', 'Chinese', '简体中文', array(
 	'Class:Cluster' => '集群',
-	'Class:ApplicationSolution' => '应用',
-	'Class:OSVersion' => '操作系统',
-
-	'Class:FunctionalCI/Attribute:solution_list' => '应用',
-	'Class:FunctionalCI/Attribute:solution_list+' => '使用该 CI 的应用',
-	'Class:BusinessProcess/Attribute:used_solution_list' => '应用',
-	'Class:BusinessProcess/Attribute:used_solution_list+' => '业务流程所依赖的应用',
-	'Class:lnkSolutionToCI/Attribute:solution_id' => '应用',
-	'Class:lnkSolutionToCI/Attribute:solution_name' => '应用',
-	'Class:lnkProcessToSolution/Attribute:solution_id' => '应用',
-	'Class:lnkProcessToSolution/Attribute:solution_name' => '应用',
-	'Menu:ApplicationSolution' => '应用',
-	'Menu:ApplicationSolution+' => '所有应用',
 	'Menu:Cluster' => '集群',
 	'Menu:Cluster+' => '所有集群',
+	'Class:Cluster/Attribute:status' => '状态',
+	'Class:Cluster/Attribute:vip_list' => '集群VIP',
+	'Class:Cluster/Attribute:vip_list+' => '该集群使用的所有VIP',
+	'Class:Cluster/Attribute:server_list' => '服务器',
+	'Class:Cluster/Attribute:server_list+' => '隶属于该集群的所有服务器',
+	
 ));
 
 // Server
 Dict::Add('ZH CN', 'Chinese', '简体中文', array(
 	'Class:Server/Attribute:name' => '主机名',
 	'Class:Server/Attribute:serialnumber' => 'SN',
+	'Class:Server/Attribute:all_ip' => 'IP地址',
+	'Class:Server/Attribute:ip_list' => 'IP地址',
+	'Class:Server/Attribute:ip_list+' => '该服务器的所有 IP地址',
+	'Class:Server/Attribute:cluster_list' => '集群',
+	'Class:Server/Attribute:cluster_list+' => '该服务器所属集群',
 ));
 
 // IPAddress
@@ -54,8 +52,12 @@ Dict::Add('ZH CN', 'Chinese', '简体中文', array(
 Dict::Add('ZH CN', 'Chinese', '简体中文', array(
 	'Class:PhysicalIP' => 'IP地址',
 	'Class:PhysicalIP/Attribute:ipaddress' => 'IP地址',
-	'Class:PhysicalIP/Attribute:connect_id' => '设备',
-	'Class:PhysicalIP/Attribute:connect_name' => '设备',
+	'Class:PhysicalIP/Attribute:connectableci_id' => '链接的设备',
+	'Class:PhysicalIP/Attribute:type' => '类型',
+	'Class:PhysicalIP/Attribute:type/Value:int' => '内网',
+	'Class:PhysicalIP/Attribute:type/Value:ext' => '外网',
+	'Class:PhysicalIP/Attribute:type/Value:oob' => '管理卡',
+	'Class:PhysicalIP/Attribute:comment' => '注释',
 	'Menu:PhysicalIP' => 'IP地址',
 	'Menu:PhysicalIP+' => '所有IP地址'
 ));
@@ -64,7 +66,16 @@ Dict::Add('ZH CN', 'Chinese', '简体中文', array(
 Dict::Add('ZH CN', 'Chinese', '简体中文', array(
 	'Class:VirtualIP' => 'VIP地址',
 	'Menu:VirtualIP' => 'VIP地址',
-	'Menu:VirtualIP+' => '所有VIP地址'
+	'Menu:VirtualIP+' => '所有VIP地址',
+	'Class:VirtualIP/Attribute:connectableci_id' => '链接的集群',
+	'Class:VirtualIP/Attribute:type' => '类型',
+	'Class:VirtualIP/Attribute:type/Value:int' => '内网',
+	'Class:VirtualIP/Attribute:type/Value:ext' => '外网',
+	'Class:VirtualIP/Attribute:type/Value:oob' => '管理卡',
+
+	'Class:VirtualIP/Attribute:comment' => '注释',
+	'Class:VirtualIP/Attribute:isp_id' => '运营商',
+
 ));
 
 // Domain
@@ -73,6 +84,9 @@ Dict::Add('ZH CN', 'Chinese', '简体中文', array(
 	'Menu:Domain' => '域名',
 	'Menu:Domain+' => '所有域名',
 	'Class:Domain/Attribute:domain' => '域名',
+	'Class:Domain/Attribute:status' => '状态',
+	'Class:Domain/Attribute:contacts' => '联系人',
+	'Class:Domain/Attribute:applicationsolution_id' => '产品线',
 
 ));
 
@@ -84,3 +98,15 @@ Dict::Add('ZH CN', 'Chinese', '简体中文', array(
 	'Class:Cluster/Error:ClusterMustBeNotNull' => '集群名称不能为空',
 	'Class:Cluster/Error:ClusterMustBeUnique' => '此集群已存在，不能重复添加',
 ));
+
+// Location
+Dict::Add('ZH CN', 'Chinese', '简体中文', array(
+	'Class:Location/Attribute:isp_id' => '运营商',
+));
+
+// ApplicationSolution
+Dict::Add('ZH CN', 'Chinese', '简体中文', array(
+	'Class:ApplicationSolution/Name' => '%1$s.%2$s',
+	'Class:ApplicationSolution/Attribute:businessprocess_id' => '业务线',
+));
+
