f40a3007d2a5e2cb9fdca4e4da4143b68706d015
annprog <tecbbs@qq.com>
2016-05-31 19:11:49 +0800

    work on ucenter登录

--- authent-ucenter/module.authent-ucenter.php
+++ authent-ucenter/module.authent-ucenter.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'ucenter/1.0.0',
+	'authent-ucenter/1.0.0',
 	array(
 		// Identification
 		//
@@ -23,7 +23,7 @@ SetupWebPage::AddModule(
 		// Components
 		//
 		'datamodel' => array(
-			'model.ucenter.php'
+			'model.authent-ucenter.php'
 		),
 		'webservice' => array(
 			
