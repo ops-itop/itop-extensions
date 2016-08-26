877bb4cfbad3c6e79e728ad03040645e65007084
annprog <tecbbs@qq.com>
2016-08-26 17:58:59 +0800

    [custom-mod] 1. WelcomeMenu下新增提工单链接

--- custom-mod/zh.dict.custom-mod.php
+++ custom-mod/zh.dict.custom-mod.php
@@ -11,6 +11,7 @@
 // Menu
 Dict::Add('ZH CN', 'Chinese', '简体中文', array(
 	'Menu:PortalUserRequest' => '提交用户请求',
+	'Menu:WelcomeRequest' => '提工单',
 	'Menu:PortalUserRequest+' => '创建一个新用户请求工单',
 	'Portal:CMDB' => '返回CMDB',
 ));
