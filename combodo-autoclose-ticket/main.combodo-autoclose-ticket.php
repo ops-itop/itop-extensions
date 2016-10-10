15b4a0d3c59b4431dcdf8e059c9a135670d319c4
annprog <tecbbs@qq.com>
2016-10-10 14:37:38 +0800

    add combodo-autoclose-ticket

---
+++ combodo-autoclose-ticket/main.combodo-autoclose-ticket.php
@@ -0,0 +1,92 @@
+<?php
+// Copyright (C) 2012 Combodo SARL
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; version 3 of the License.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the Free Software
+//   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+/**
+ * Module combodo-autoclose-ticket
+ *
+ * @author      Erwan Taloc <erwan.taloc@combodo.com>
+ * @author      Romain Quetiez <romain.quetiez@combodo.com>
+ * @author      Denis Flaven <denis.flaven@combodo.com>
+ * @license     http://www.opensource.org/licenses/gpl-3.0.html LGPL
+ */
+
+
+class AutoCloseTicket implements iBackgroundProcess
+{
+	public function GetPeriodicity()
+	{	
+		return 86400; // Once a day
+	}
+
+	public function Process($iTimeLimit)
+	{
+
+      		$aReport = array();
+		if (MetaModel::IsValidClass('UserRequest'))
+		{
+			// Get Resolved user request to be closed automatically according to request_closure_delay set for the customer
+			$iAutoCloseDelay = MetaModel::GetModuleSetting('combodo-autoclose-ticket', 'userrequest_autoclose_delay', '');
+			$oSetUserRequest = new DBObjectSet(DBObjectSearch::FromOQL("SELECT UserRequest AS u WHERE u.status = 'resolved' AND u.resolution_date <= DATE_SUB(NOW(), INTERVAL $iAutoCloseDelay DAY)"));
+			while ((time() < $iTimeLimit) && $oToClose = $oSetUserRequest->Fetch())
+			{
+				$oToClose->ApplyStimulus('ev_close');
+				//$oToEscalate->Set('tto_escalation_deadline', null);
+				$oToClose->DBUpdate();
+				$aReport['reached Auto close deadline'][] = $oToClose->Get('ref');
+			}
+		}
+
+		if (MetaModel::IsValidClass('Incident'))
+		{
+			// Get Resolved incident to be closed automatically according to incident_closure_delay set for the customer
+			$iAutoCloseDelay = MetaModel::GetModuleSetting('combodo-autoclose-ticket', 'incident_autoclose_delay', '');
+			$oSetIncident = new DBObjectSet(DBObjectSearch::FromOQL("SELECT Incident AS i WHERE i.status = 'resolved' AND i.resolution_date <= DATE_SUB(NOW(), INTERVAL $iAutoCloseDelay DAY)"));
+			while ((time() < $iTimeLimit) && $oToClose = $oSetIncident->Fetch())
+			{
+				$oToClose->ApplyStimulus('ev_close');
+				//$oToEscalate->Set('tto_escalation_deadline', null);
+				$oToClose->DBUpdate();
+				$aReport['reached Auto close deadline'][] = $oToClose->Get('ref');
+			}
+		}
+		
+
+		$aStringReport = array();
+		foreach ($aReport as $sOperation => $aTicketRefs)
+		{
+			if (count($aTicketRefs) > 0)
+			{
+				$aStringReport[] = $sOperation.': '.count($aTicketRefs).' {'.implode(', ', $aTicketRefs).'}';
+			}
+		}
+		if (count($aStringReport) == 0)
+		{
+			return "No ticket to process";
+			echo "No ticket to process";
+		}
+		else
+		{
+
+			return "Some tickets were closed - ".implode('; ', $aStringReport);
+			echo "Some tickets were closed - ".implode('; ', $aStringReport);
+
+		}
+	}
+}
+
+
+?>
---
+++ combodo-autoclose-ticket/module.combodo-autoclose-ticket.php
@@ -0,0 +1,65 @@
+<?php
+// Copyright (C) 2012 Combodo SARL
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; version 3 of the License.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the Free Software
+//   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+SetupWebPage::AddModule(
+	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+	'combodo-autoclose-ticket/1.0.0',
+	array(
+		// Identification
+		//
+		'label' => 'Auto closure of Incident and UserRequest',
+		'category' => 'feature',
+
+		// Setup
+		//
+		'dependencies' => array(
+
+		),
+		'mandatory' => false,
+		'visible' => true,
+
+		// Components
+		//
+		'datamodel' => array(
+			'main.combodo-autoclose-ticket.php'
+		),
+		'webservice' => array(
+			
+		),
+		'data.struct' => array(
+			// add your 'structure' definition XML files here,
+		),
+		'data.sample' => array(
+			// add your sample data XML files here,
+		),
+		
+		// Documentation
+		//
+		'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
+		'doc.more_information' => '', // hyperlink to more information, if any 
+
+		// Default settings
+		//
+		'settings' => array(
+			// Module specific settings go here, if any
+			'incident_autoclose_delay' => '7',
+			'userrequest_autoclose_delay' => '7',
+		),
+	)
+);
+
+
+?>
