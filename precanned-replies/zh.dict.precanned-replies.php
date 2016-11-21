21b9cd1fb4246927d70e1aa0bed1fb10d9cb84b0
annprog <tecbbs@qq.com>
2016-11-21 15:21:28 +0800

    [precanned-replies]增加简体中文

---
+++ precanned-replies/zh.dict.precanned-replies.php
@@ -0,0 +1,42 @@
+<?php
+// Copyright (C) 2010 Combodo SARL
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; version 3 of the License.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the Free Software
+//   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+/**
+ * Localized data
+ *
+ * @author      Erwan Taloc <erwan.taloc@combodo.com>
+ * @author      Romain Quetiez <romain.quetiez@combodo.com>
+ * @author      Denis Flaven <denis.flaven@combodo.com>
+ * @license     http://www.opensource.org/licenses/gpl-3.0.html LGPL
+ */
+
+Dict::Add('ZH CN', 'Chinese', '简体中文', array(
+	'Menu:PrecannedReplies' => '预定义回复',
+	'Menu:PrecannedReplies+' => '预定义回复',
+
+	'UI:Button-AddReply' => '预定义回复模板',
+	'UI:Dlg-PickAReply' => '选择一个模板',
+	
+	'Class:PrecannedReply' => '预定义回复',
+	'Class:PrecannedReply+' => '预定义回复',
+	'Class:PrecannedReply/Attribute:name' => '名称',
+	'Class:PrecannedReply/Attribute:name+' => '',
+	'Class:PrecannedReply/Attribute:description' => '描述',
+	'Class:PrecannedReply/Attribute:description+' => '',
+	'Class:PrecannedReply/Attribute:body' => '内容',
+	'Class:PrecannedReply/Attribute:body+' => '',
+));
+?>
