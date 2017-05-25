d4b5b42724570dab78d941caede25bf9196efd11
annprog <tecbbs@qq.com>
2017-05-25 17:01:03 +0800

    [itop-request-template-modify] 实现js显示帮助信息

--- itop-request-template-modify/README.md
+++ itop-request-template-modify/README.md
@@ -24,3 +24,20 @@
 ## 事件管理工单
 工单模板
  - 只有一个code: functionalcis_list(事件涉及的APP)。应在onchecktowrite函数中将其写入工单functionalcis_list，这样action就能直接通过functionalcis_list来指派工单（考虑到从rest api创建工单的情况，这样做能统一action行为）
+
+## 工单提示信息
+模板的description字段如果包含 "提示</label>" ，会将description字段输出到工单创建表单用户数据前面，可以作为提示信息，支持HTML，例如:
+
+```
+<div id="template_tips" class="portal_form_field form_field"><div class="form-group"><label class="control-label">提示</label><div class="help-block"></div><p>域名-集群对应规则表</p><table><tr><td>*.online.com</td><td>Online</td></tr></table></div></div>
+```
+
+## template-base补丁
+如果需要使用description显示提示信息，建议打补丁 `template_base_3.0.4.patch`，将description字段改为AttributeText类型，支持更长的html代码（AttributeString类型255个字节不够用）
+
+打补丁命令:
+
+```
+cd extensions/itop-request-template-modify/
+patch -p1 < template_base_3.0.4.patch
+ ```
--- itop-request-template-modify/display_control.js
+++ itop-request-template-modify/display_control.js
@@ -1,6 +1,7 @@
 $().ready(function(){
 	$("div[data-field-id='request_template_ip_textarea']").attr("style", "display:none");
 	displayTipsButton();
+	displayTips();
 });
 
 function displayList()
@@ -18,4 +19,14 @@ function displayTipsButton()
 {
 	var button = '<div class="form-group"><label class="control-label">关联的服务器<\/label><div class="help-block"><\/div><div class="btn-group"><button class="btn btn-primary form_btn_transition" type="button" name="le_server_list" onclick="displayList()"><span class="glyphicon glyphicon-list"><\/span>  &nbsp;&nbsp;从列表勾选机器<\/button><button class="btn btn-info form_btn_transition" type="button" name="le_server_textarea" onclick="displayTextarea()"><span class="glyphicon glyphicon-text-height"><\/span>  &nbsp;&nbsp;直接输入IP地址<\/button><\/div><\/div>'
 	$("div[data-field-id=request_template_ip_textarea]").before(button);
+}
+
+function displayTips()
+{
+	var inputval = $("div[data-field-id='template_data']").find("input").val();
+	var tips = $.parseJSON(inputval);
+	if(tips.description.indexOf("提示</label>") > 0)
+	{
+		$("div[data-field-id='user_data']").before(tips.description);
+	}
 }
