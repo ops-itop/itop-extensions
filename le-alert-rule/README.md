f3d72f29d4a88d1a9be4b147f86808fd6b604276
annprog <tecbbs@qq.com>
2017-06-30 19:00:49 +0800

    [le-alert-rule]定义报警规则类

---
+++ le-alert-rule/README.md
@@ -0,0 +1,3 @@
+# le-alert-rule
+
+使用iTop管理报警自定义阈值规则
---
+++ le-alert-rule/zh.dict.le-alert-rule.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+// Model
+Dict::Add('ZH CN', 'Chinese', '简体中文', array(
+	'Class:Brand' => '厂商',
+	'Class:Model' => '型号',
+	'Class:Model/Name' => '%1$s/%2$s',
+	
+));
