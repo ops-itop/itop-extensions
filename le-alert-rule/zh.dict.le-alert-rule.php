8cbf8efe6e3c508b3c3a725f02d6a996948d9e24
annprog <tecbbs@qq.com>
2017-07-13 15:56:22 +0800

    [le-alert-rule] 更新翻译

--- le-alert-rule/zh.dict.le-alert-rule.php
+++ le-alert-rule/zh.dict.le-alert-rule.php
@@ -6,10 +6,38 @@
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
-// Model
+// AlertType
 Dict::Add('ZH CN', 'Chinese', '简体中文', array(
-	'Class:Brand' => '厂商',
-	'Class:Model' => '型号',
-	'Class:Model/Name' => '%1$s/%2$s',
-	
+	'Class:AlertType' => '报警类型',
+	'Menu:AlertType' => '报警类型',
+	'Menu:AlertType+' => '所有报警类型',
+	'Class:AlertType/Attribute:description' => '描述',
+	'Class:AlertType/Attribute:alertrule_list' => '报警规则',
+	'Class:AlertType/Attribute:alertrule_list+' => '此类型链接的所有报警规则',
+));
+
+// AlertRule
+Dict::Add('ZH CN', 'Chinese', '简体中文', array(
+	'Class:AlertRule' => '报警规则',
+	'Menu:AlertRule' => '报警规则',
+	'Menu:AlertRule+' => '所有报警规则',
+	'Class:AlertRule/Attribute:alerttype_id' => '报警类型',
+	'Class:AlertRule/Attribute:method' => '报警方法',
+	'Class:AlertRule/Attribute:method/Value:qoq_fall' => '环比下降',
+	'Class:AlertRule/Attribute:method/Value:qoq_rise' => '环比上涨',
+	'Class:AlertRule/Attribute:method/Value:qps' => '每秒请求量',
+	'Class:AlertRule/Attribute:method/Value:ratio' => '占总请求量的比重',
+	'Class:AlertRule/Attribute:qoq_cycle' => '周期',
+	'Class:AlertRule/Attribute:threshold' => '阈值',
+	'Class:AlertRule/Attribute:functionalcis_list' => '功能配置项',
+	'Class:AlertRule/Attribute:functionalcis_list+' => '此规则链接的所有功能配置项',
+));
+
+// lnkFunctionalCIToAlertRule
+Dict::Add('ZH CN', 'Chinese', '简体中文', array(
+	'Class:lnkFunctionalCIToAlertRule' => '功能配置项/报警规则',
+	'Class:lnkFunctionalCIToAlertRule/Attribute:functionalci_id' => '功能配置项',
+	'Class:lnkFunctionalCIToAlertRule/Attribute:alertrule_id' => '报警规则',
+	'Menu:lnkFunctionalCIToAlertRule' => '功能配置项/报警规则',
+	'Menu:lnkFunctionalCIToAlertRule+' => '所有 功能配置项/报警规则',
 ));
