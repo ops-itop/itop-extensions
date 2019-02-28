0c021bd62c65c0774f2ac1492020979c1c0aad14
Annhe <tecbbs@qq.com>
2019-02-28 17:25:10 +0800

    [patches] add run.sh

---
+++ patches/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+############################
+# Usage:
+# File Name: run.sh
+# Author: annhe  
+# Mail: i@annhe.net
+# Created Time: 2019-02-28 17:17:00
+############################
+cp *.patch ../../
+cd ../../
+for id in `cat *.patch |grep "^+++" |awk '{print $2}' |sed -r 's/(web|b)\///g'`;do
+	dos2unix $id
+done
+
+for id in `ls *.patch`;do
+	patch -p1 --binary -l -N < $id
+done
