052c05d1549c22b15d8b8c969f9f5a801baba15b
Annhe <tecbbs@qq.com>
2019-02-28 17:31:12 +0800

    [patches] add run.sh

--- patches/run.sh
+++ patches/run.sh
@@ -8,6 +8,8 @@
 # Created Time: 2019-02-28 17:17:00
 ############################
 cp *.patch ../../
+cp -ru js ../../
+cp -ru css ../../
 cd ../../
 for id in `cat *.patch |grep "^+++" |awk '{print $2}' |sed -r 's/(web|b)\///g'`;do
 	dos2unix $id
